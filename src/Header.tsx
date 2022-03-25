f4fcd511fa48a9f3790be54e2dc14c25c498ee33
Pavel Morozuk <morozuki@mail.ru>
2022-03-26 00:56:37 +0300

    Create

---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>CSS</li>
+                <li>HTML</li>
+                <li>REACT</li>
+                <li>JS</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Technologies;
