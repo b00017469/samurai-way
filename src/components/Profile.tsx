7bfcc8908f0bb4aaf3c4f428c97757d4329b518a
Pavel Morozuk <morozuki@mail.ru>
2022-04-10 22:23:50 +0300

    samuray way 14

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <div>
             <img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt=""/>
         </div>
@@ -15,10 +15,10 @@ const Profile = () => {
                 new post
             </div>
             <div>
-                <div>
+                <div className={s.item}>
                     post 1
                 </div>
-                <div>
+                <div className={s.item}>
                     post 2
                 </div>
             </div>
