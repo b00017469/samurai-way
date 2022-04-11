cddbd023c6407e23e79c674bc50c7c7719f8a5b9
Pavel Morozuk <morozuki@mail.ru>
2022-04-11 16:56:19 +0300

    samuray way 2

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,40 @@
 import React from "react";
-import s from 'Dialogs.module.css'
+import s from './Dialogs.module.css'
 
 const Dialogs = () =>{
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Masha
+                </div>
+                <div className={s.dialog}>
+                    Pasha
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Tosha
+                </div>
+                <div className={s.dialog}>
+                    Vova
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hello!
+                </div>
+                <div className={s.message}>
+                    How are you?
+                </div>
+                <div className={s.message}>
+                    Be be be!!!
+                </div>
+            </div>
         </div>
     )
 }
