12df636cfe458a9bc50846b772f9c13002413252
Pavel Morozuk <morozuki@mail.ru>
2022-04-11 15:41:01 +0300

    samuray way 19

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import s from './Navbar.module.css'
+import {Settings} from "../Settings/Settings";
 
 const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <a>Profile</a>
+            <a href="/profile">Profile</a>
         </div>
         <div className={s.item}>
-            <a>Messages</a>
+            <a href="/dialogs">Messages</a>
         </div>
         <div className={s.item}>
-            <a>News</a>
+            <a href="/news">News</a>
         </div>
         <div className={s.item}>
-            <a>Music</a>
+            <a href="/music">Music</a>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>Settings</a>
+            <a href="/settings">Settings</a>
         </div>
     </nav>
 }
